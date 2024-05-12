a56d15a176a39684b2a2937307712209a88b1271
ark4diem <jasonusotelo@gmail.com>
2024-05-12 02:03:15 -0600

    Final Begin

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient} from "@libsql/client";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
+
+dotenv.config()
 
 const port = process.env.PORT ?? 3000
 
@@ -15,15 +19,51 @@ const io = new Server(server , {
     }
 })
 
-io.on("connection", (socket) => {
+const db = createClient({
+  url: process.env.DB_URL,
+  authToken: process.env.DB_TOKEN,
+})
+
+await db.execute(""+"CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT, user TEXT)")
+
+io.on("connection", async (socket) => {
   console.log('a user has connected!')
 
   socket.on('disconnect', () => {
     console.log('a user has disconnected!')
   })
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg)
+  socket.on('chat message', async (msg) => {
+    let result
+      const username = socket.handshake.auth.username ?? "Anonymous"
+    try {
+
+      result = await db.execute({
+          sql: "INSERT INTO messages (content, user) VALUES (:msg, :username)",
+          args: { msg, username },
+      })
+    } catch (error) {
+      console.error(error)
+        return
+    }
+    io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
   })
+
+    console.log('auth')
+    console.log(socket.handshake.auth)
+  if (!socket.recovered) {
+    try {
+        const result = await db.execute({
+            sql: "SELECT id, content, user FROM messages WHERE id > ?",
+            args: [socket.handshake.auth.serverOffset ?? 0],
+        })
+        result.rows.forEach((row) => {
+            socket.emit('chat message', row.content, row.id.toString(), row.user)
+        })
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
 })
 
 app.use(logger('dev'))
